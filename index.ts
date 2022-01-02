e570f801662d17be26b186299b63586065cae5d9
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2022-01-02 16:43:22 +0100

    hello word

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { Midi } from '@tonejs/midi'
+import * as fs from 'fs'
+
+// create a new midi file
+var midi = new Midi()
+// add a track
+const track = midi.addTrack()
+track
+  .addNote({
+    midi: 60,
+    time: 0,
+    duration: 0.2,
+  })
+  .addNote({
+    name: 'C5',
+    time: 0.3,
+    duration: 0.1,
+  })
+  .addCC({
+    number: 64,
+    value: 127,
+    time: 0.2,
+  })
+
+// write the output
+fs.writeFileSync('output.mid', new Buffer(midi.toArray()))
