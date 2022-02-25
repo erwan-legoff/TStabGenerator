5d34b05f41b320d6c73256d1ef1d329d094683cc
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2022-02-25 21:41:35 +0100

    add other ideas

--- src/instruments/fretBoards/fretBoard.ts
+++ src/instruments/fretBoards/fretBoard.ts
@@ -9,3 +9,5 @@ export interface fretBoard {
 //* implement fretBoard . En gros on veut qu'un instrument ait un fretboard et qu'un fretboard soit initialisé grace à un tuning avec un tableau de tabLines, c'est en fait le fretboard qui va recevoir les tabNotes de la tablature.
 //*C'est la tablature qui aura le contrôle de la position des tabNotes grâce à la stratégie de choix des string line, mais aussi grâce à la stratégie de choix des fret (à ajouter)
 //? Est ce qu'une tabline deviendrait une corde ?
+//? Dans le futur une tablature pourrait être composé de "measureTab" qui serait chacune une seule mesure de tablature.
+//? Cela permettrait de facilement gérer le graphisme de la tablature et d'incorporer une notion de temps.
