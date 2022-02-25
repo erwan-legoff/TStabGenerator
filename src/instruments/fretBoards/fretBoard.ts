bb084c55d53ae77215a3a02cbd3de20bac76eb80
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2022-02-25 21:30:58 +0100

    WIP¨add todos and brainstorm in the fretBoard

--- src/instruments/fretBoards/fretBoard.ts
+++ src/instruments/fretBoards/fretBoard.ts
@@ -5,3 +5,7 @@ export interface fretBoard {
   getName(): string
   getEmptyTabLines(): TabLine[]
 }
+
+//* implement fretBoard . En gros on veut qu'un instrument ait un fretboard et qu'un fretboard soit initialisé grace à un tuning avec un tableau de tabLines, c'est en fait le fretboard qui va recevoir les tabNotes de la tablature.
+//*C'est la tablature qui aura le contrôle de la position des tabNotes grâce à la stratégie de choix des string line, mais aussi grâce à la stratégie de choix des fret (à ajouter)
+//? Est ce qu'une tabline deviendrait une corde ?
