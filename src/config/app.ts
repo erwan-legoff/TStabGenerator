6f694efd152088a08b008be25d0e80219ef8b571
erwan-legoff <erwan.le-goff@etu.umontpellier.fr>
2023-05-23 20:23:33 +0200

    logs

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import { apiGetMelody } from '../controllers/MelodyController'
+import morgan from 'morgan'
 
 dotenv.config()
 
 const app = express()
 app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 app.get('/melody', apiGetMelody)
 
